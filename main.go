61867f16b4016da23a8d078b52dd8395df259fd6
coyove <coyove@hotmail.com>
2018-09-11 23:14:25 +0800

    Update code WIP

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	_html "html"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,6 +15,7 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
 	"golang.org/x/text/encoding/japanese"
@@ -21,20 +25,25 @@ import (
 	"golang.org/x/text/transform"
 )
 
-var solrEndpoint = "http://127.0.0.1:8983/solr/new_core/update/json/docs"
+var solrEndpoint = "http://127.0.0.1:8983/solr/new_core/update/json/docs?commit=true"
+
+const maxResponseSize = 5 * 1024 * 1024
 
 func SHA1ForGUID(in string) string {
 	x := sha1.Sum([]byte(in))
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x", x[:4], x[4:6], x[6:8], x[8:10], x[10:16])
 }
 
-func CleanText(in string) string {
-	return regexp.MustCompile(`(\n|\t|\s+)`).ReplaceAllString(in, " ")
+func IsChinese(r rune) bool {
+	return unicode.Is(unicode.Scripts["Han"], r)
 }
 
-type Link struct {
-	Title string `json:"title"`
-	URL   string `json:"url"`
+func IsJapanese(r rune) bool {
+	return unicode.Is(unicode.Scripts["Hiragana"], r) || unicode.Is(unicode.Scripts["Katakana"], r)
+}
+
+func CleanText(in string) string {
+	return regexp.MustCompile(`(\n|\t|\s+)`).ReplaceAllString(in, " ")
 }
 
 type Page struct {
@@ -48,8 +57,10 @@ type Page struct {
 	H2          []string `json:"h2"`
 	H3          []string `json:"h3"`
 	H4          []string `json:"h4"`
-	Links       []Link   `json:"links"`
-	Content     string   `json:"content"`
+	Links       []string `json:"links"`
+	ContentEN   string   `json:"content_txt_en"`
+	ContentCN   string   `json:"content_txt_cn"`
+	ContentJP   string   `json:"content_txt_jp"`
 }
 
 func JoinURL(baseurl, url2 string) string {
@@ -112,6 +123,99 @@ REDO:
 	return x
 }
 
+func ExtractPlainText(html []byte) (string, string, string) {
+	r := bufio.NewReader(bytes.NewReader(html))
+	en, cn, jp := bytes.Buffer{}, bytes.Buffer{}, bytes.Buffer{}
+	readUntil := func(end rune) (string, bool, string) {
+		tmp := bytes.Buffer{}
+		tag := ""
+		b1, _ := r.Peek(1)
+		endtag := len(b1) > 0 && b1[0] == '/'
+
+		for {
+			b, _, err := r.ReadRune()
+			if err != nil {
+				break
+			}
+			tmp.WriteRune(b)
+			if b == end {
+				break
+			}
+			if b == ' ' && tag == "" {
+				tag = tmp.String()
+			}
+		}
+
+		if tmp.Len() == 0 || tmp.Bytes()[tmp.Len()-1] != byte(end) {
+			return "", false, ""
+		}
+
+		if tag == "" {
+			tag = tmp.String()[:tmp.Len()-1]
+		}
+
+		if endtag {
+			tag = tag[1:]
+		}
+		return strings.ToLower(tag), endtag, tmp.String()
+	}
+	writeSpace := func(buf *bytes.Buffer) {
+		if buf.Len() > 0 {
+			if buf.Bytes()[buf.Len()-1] == ' ' {
+				return
+			}
+		}
+		buf.WriteRune(' ')
+	}
+
+	tagstack := make([]string, 0)
+	for {
+		r, _, err := r.ReadRune()
+		if err != nil {
+			break
+		}
+
+		if r == '<' {
+			tag, endtag, raw := readUntil('>')
+			if endtag {
+				i := len(tagstack) - 1
+				for ; i >= 0; i-- {
+					if tagstack[i] == tag {
+						break
+					}
+				}
+				if i >= 0 {
+					tagstack = tagstack[:i]
+				}
+				continue
+			}
+			if len(raw) > 512 {
+				en.WriteRune(r)
+				en.WriteString(raw)
+				continue
+			}
+			tagstack = append(tagstack, tag)
+			continue
+		}
+
+		if r == ' ' || r == '\t' || r == '\n' || r == '\r' {
+			writeSpace(&jp)
+			writeSpace(&cn)
+			writeSpace(&en)
+		} else if IsJapanese(r) {
+			jp.WriteRune(r)
+		} else if IsChinese(r) {
+			cn.WriteRune(r)
+			jp.WriteRune(r)
+		} else {
+			en.WriteRune(r)
+			cn.WriteRune(r)
+			jp.WriteRune(r)
+		}
+	}
+	return _html.UnescapeString(en.String()), _html.UnescapeString(cn.String()), _html.UnescapeString(jp.String())
+}
+
 func add(json []byte) {
 	req, _ := http.NewRequest("POST", solrEndpoint, bytes.NewReader(json))
 	req.Header.Add("Content-Type", "application/json")
@@ -127,12 +231,12 @@ func add(json []byte) {
 	log.Println(string(buf))
 }
 
-func walk(baseurl string, buf []byte) {
+func walk(baseurl string, buf []byte) []string {
 	charset := "utf-8"
 	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(buf))
 	if err != nil {
 		log.Println(err)
-		return
+		return nil
 	}
 
 	metacharset := doc.Find("meta[charset]").First()
@@ -190,12 +294,12 @@ SKIP:
 	doc, err = goquery.NewDocumentFromReader(bytes.NewReader(buf))
 	if err != nil {
 		log.Println(err)
-		return
+		return nil
 	}
 
 	p := Page{}
 	p.ID = SHA1ForGUID(baseurl)
-	p.Content = CleanText(doc.Text())
+	p.ContentEN, p.ContentCN, p.ContentJP = ExtractPlainText(buf)
 	p.URL = baseurl
 	p.Updated = uint32(time.Now().Unix())
 	if title := doc.Find("title").First(); title != nil {
@@ -217,52 +321,75 @@ SKIP:
 	p.H4 = make([]string, 0)
 	doc.Find("h4").Each(func(i int, s *goquery.Selection) { p.H4 = append(p.H4, s.Text()) })
 
-	p.Links = make([]Link, 0)
+	p.Links = make([]string, 0)
+	links := make([]string, 0)
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		l := Link{}
 		href, ok := s.Attr("href")
-		if !ok {
-			return
+		if ok && !strings.HasPrefix(href, "javascript:") {
+			links = append(links, JoinURL(baseurl, href))
 		}
-		l.Title = s.Text()
-		l.URL = JoinURL(baseurl, href)
-		p.Links = append(p.Links, l)
+		p.Links = append(p.Links, s.Text())
 	})
 
 	j, _ := json.Marshal(&p)
 	add(j)
+	// log.Println(string(j))
+	// log.Println(p.ContentCN)
+	return links
 }
 
-func crawl(uri string) {
+func crawl(uri string) []string {
 	_up, _ := url.Parse("http://127.0.0.01:8100")
 	c := &http.Client{
 		Transport: &http.Transport{
 			Proxy: http.ProxyURL(_up),
 		},
 	}
+	c = http.DefaultClient
 	req, _ := http.NewRequest("GET", uri, nil)
 	resp, err := c.Do(req)
 	if err != nil {
 		log.Println(err)
-		return
+		return nil
 	}
 	defer resp.Body.Close()
 
-	buf, err := ioutil.ReadAll(resp.Body)
+	buf, err := ioutil.ReadAll(io.LimitReader(resp.Body, maxResponseSize))
 	if err != nil {
 		log.Println(err)
-		return
+		return nil
 	}
 
 	if len(buf) == 0 {
 		log.Println("empty")
-		return
+		return nil
 	}
 
-	walk(uri, buf)
+	score := 0
+	for _, b := range buf {
+		if b > 0xf0 {
+			score++
+		}
+	}
+
+	if score > len(buf)/20 {
+		log.Println("maybe binary?")
+		return nil
+	}
+
+	return walk(uri, buf)
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
-	crawl("http://news.ycombinator.com")
+
+	links := []string{("http://zhstatic.zhihu.com/assets/zhihu/publish-license.jpg")}
+
+	for len(links) > 0 {
+		x := links[0]
+		log.Println(x)
+		links = links[1:]
+		links = append(links, crawl(x)...)
+		time.Sleep(time.Second)
+	}
 }
